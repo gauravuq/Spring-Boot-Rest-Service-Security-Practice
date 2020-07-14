65a80183f2bd761075c0283b908d7680716292cd
Gaurav Sood <g.sood@uqconnect.edu.au>
2020-07-14 15:03:06 +1000

    Basic Authentication !!!

---
+++ spring-security-basic-authentication/Readme.md
@@ -0,0 +1,5 @@
+## Basic Authentication Demo 
+* **This demo represents how to set up basic authentication for multiple RestMappings 
+ using database based authentication provider**
+* **H2 database has been used for quick demonstrations**
+* **"localhost:8080/hello"  url will only allow users which give valid credentials for basic authentication. The provided credentials are checked against the database entries**
--- spring-security-basic-authentication/src/main/java/com/gaurav/spring/security/springsecuritybasicauthentication/SpringSecurityBasicAuthenticationApplication.java
+++ spring-security-basic-authentication/src/main/java/com/gaurav/spring/security/springsecuritybasicauthentication/SpringSecurityBasicAuthenticationApplication.java
@@ -3,11 +3,15 @@ package com.gaurav.spring.security.springsecuritybasicauthentication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class SpringSecurityBasicAuthenticationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityBasicAuthenticationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityBasicAuthenticationApplication.class, args);
+    }
 
 }
