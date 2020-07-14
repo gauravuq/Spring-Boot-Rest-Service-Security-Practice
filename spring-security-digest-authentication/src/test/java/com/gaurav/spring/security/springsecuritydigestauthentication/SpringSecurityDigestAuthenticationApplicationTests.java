69eb993c59388ff7e2f806d63b3c8d48d7b2786b
Gaurav Sood <g.sood@uqconnect.edu.au>
2020-07-14 12:46:44 +1000

    Digest Authentication First

---
+++ spring-security-digest-authentication/src/test/java/com/gaurav/spring/security/springsecuritydigestauthentication/SpringSecurityDigestAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaurav.spring.security.springsecuritydigestauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDigestAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
