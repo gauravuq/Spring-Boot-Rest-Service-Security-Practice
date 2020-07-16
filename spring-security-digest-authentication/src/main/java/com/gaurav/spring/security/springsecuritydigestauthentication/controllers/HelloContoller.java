63d86834b13dbc1af8ba96914903f39c3f422c54
Gaurav Sood <g.sood@uqconnect.edu.au>
2020-07-16 17:02:26 +1000

    first digest authentication push

--- spring-security-digest-authentication/src/main/java/com/gaurav/spring/security/springsecuritydigestauthentication/controllers/HelloContoller.java
+++ spring-security-digest-authentication/src/main/java/com/gaurav/spring/security/springsecuritydigestauthentication/controllers/HelloContoller.java
@@ -1,4 +1,4 @@
-package com.gaurav.spring.security.springsecuritybasicauthentication.controllers;
+package com.gaurav.spring.security.springsecuritydigestauthentication.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
